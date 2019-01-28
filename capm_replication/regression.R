8a46f54e3782014dc8b363b7d4f25134d8c9b321
Jason <jasonyang19923@gmail.com>
2019-01-27 20:49:23 -0600

    update capm replication project

--- capm_replication/regression.R
+++ capm_replication/regression.R
@@ -10,9 +10,41 @@ returns_data = read_feather(paste0(basepath, "clean_returns_data.feather"))
 beta_return_data = left_join(returns_data, beta_data)
 
 # Deciles -----------------------------------------------------------------
-# divide betas all into deciles
-beta_returns_data = mutate(beta_returns_data, decile = ntile(beta, 10))
+# divide betas all into deciles and add mkt cap
+beta_return_data = mutate(beta_return_data, Decile = ntile(beta, 10),
+                          MktCap = Price * ShsOutstand)
+beta_return_data = filter(beta_return_data, !is.na(beta), !is.na(MktCap), !is.na(Return))
 
-# run regression
+# summarise what the beta buckets look like
+beta_summary_stats = group_by(beta_return_data, Decile) %>%
+  summarise(avg_beta = mean(beta),
+            standard_dev = sd(beta),
+            max_beta = max(beta),
+            min_beta = min(beta),
+            avg_mk_cap = mean(ShsOutstand * mean(Price, na.rm = TRUE), na.rm = TRUE))
 
-# do plot
+# market cap vs beta decile
+mkcap_vs_beta_dec = ggplot(beta_summary_stats, aes(x = decile, avg_mk_cap)) +
+  geom_col()
+
+# Portfolio ---------------------------------------------------------------
+# need to change the beta returns data so we have an average return
+# weighted by market cap for each date observation
+decile_return_data = group_by(beta_return_data, Decile, Date) %>%
+  summarise(weighted_return = weighted.mean(Return, MktCap)) %>%
+  mutate(weighted_return = weighted_return + 1)
+
+# starting with $10,000 in 1935 for each beta bucket
+# see the value of the portfolio over time
+initial_port_value = 100000
+
+make_portfolio <- function(decile)
+{
+  portfolio_decile = filter(decile_return_data, Decile == decile)
+  portfolio_decile = mutate(portfolio_decile, 
+                            portfolio_value = cumprod(c(initial_port_value, weighted_return)[-1]))
+  return(portfolio_decile)
+}
+
+portfolio_list = lapply(1:10,
+                        make_portfolio)
